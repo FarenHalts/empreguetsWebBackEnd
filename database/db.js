c1249c5cc28387ed4d4e55f0b35239e277cce1b3
Lucas Garcia <lucas.garcia@roit.com.br>
2020-12-07 20:30:28 -0300

    remove port

--- database/db.js
+++ database/db.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 module.exports = {
     connection: () => {
         return mysql.createConnection({
-            port: "3306",
             host: "35.198.50.41",
             user: "empreguets",
             database: "db_empreguets",
