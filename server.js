2adbc8c839b7122d3a6c609a48230a5856d11788
Lucas Garcia <lucas.garcia@roit.com.br>
2020-12-07 00:08:43 -0300

    remove cors

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 var express = require("express")
 const consign = require('consign');
-var cors = require("cors")
 
 var app = express()
 
 require('dotenv').config()
 
 var port = process.env.PORT || 5000
-app.use(cors())
+
 consign()
     .include('./middleware/global')
     .then('routes')
